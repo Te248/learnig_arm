816506f2d5194fdceb7d700409cdfd2b1cd230ef
Thuan <pmttmp24@gmail.com>
2019-12-15 23:54:29 +0700

    lan 3 ngon lanh

---
+++ Template/build/bootloader/efuse/esp32/esp_efuse_table.d
@@ -0,0 +1,30 @@
+esp32/esp_efuse_table.o: \
+ /home/thuan/esp/esp-idf/components/efuse/esp32/esp_efuse_table.c \
+ /home/thuan/Documents/Git_stm32f4/Template/build/bootloader/../include/sdkconfig.h \
+ /home/thuan/esp/esp-idf/components/efuse/include/esp_efuse.h \
+ /home/thuan/esp/esp-idf/components/esp32/include/esp_err.h \
+ /home/thuan/esp/esp-idf/components/log/include/esp_log.h \
+ /home/thuan/esp/esp-idf/components/esp32/include/rom/ets_sys.h \
+ /home/thuan/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /home/thuan/esp/esp-idf/components/esp32/include/esp_assert.h \
+ /home/thuan/esp/esp-idf/components/log/include/esp_log_internal.h \
+ /home/thuan/esp/esp-idf/components/efuse/esp32/include/esp_efuse_table.h
+
+/home/thuan/Documents/Git_stm32f4/Template/build/bootloader/../include/sdkconfig.h:
+
+/home/thuan/esp/esp-idf/components/efuse/include/esp_efuse.h:
+
+/home/thuan/esp/esp-idf/components/esp32/include/esp_err.h:
+
+/home/thuan/esp/esp-idf/components/log/include/esp_log.h:
+
+/home/thuan/esp/esp-idf/components/esp32/include/rom/ets_sys.h:
+
+/home/thuan/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+/home/thuan/esp/esp-idf/components/esp32/include/esp_assert.h:
+
+/home/thuan/esp/esp-idf/components/log/include/esp_log_internal.h:
+
+/home/thuan/esp/esp-idf/components/efuse/esp32/include/esp_efuse_table.h:
+/home/thuan/esp/esp-idf/components/efuse/esp32/esp_efuse_table.c:
---
+++ Template/build/cxx/cxx_exception_stubs.d
@@ -0,0 +1,6 @@
+cxx_exception_stubs.o: \
+ /home/thuan/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
+ /home/thuan/Documents/Git_stm32f4/Template/build/include/sdkconfig.h
+
+/home/thuan/Documents/Git_stm32f4/Template/build/include/sdkconfig.h:
+/home/thuan/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
