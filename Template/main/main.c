fcb106d2bd29a3ad0a014fdc6e2337054a301e7d
Thuan <pmttmp24@gmail.com>
2019-12-17 00:52:28 +0700

    i2c

--- Template/main/main.c
+++ Template/main/main.c
@@ -1,10 +1,10 @@
 
 #include "driver/gpio.h"
-#include "driver/uart.h"
 #include "freertos/queue.h"
 #include "string.h"
 #include "freertos/task.h"
 #include "driver/i2c.h"
+#include "freertos/FreeRTOS.h"
 
 #define ESP_SLAVE_ADDR 0x68 
 #define WRITE_BIT  I2C_MASTER_WRITE /*!< I2C master write */
@@ -15,12 +15,17 @@
 #define NACK_VAL   0x1         /*!< I2C nack value */
 
 uint8_t second,minute,hour,day,date,month,year;
-void bcd_to_dec(uint8_t number)
+
+int bcd_to_dec(uint8_t number)
 {
     return((number/16*10)+(number%16));
 }
+int dec_to_bcd(uint8_t number)
+{
+    return ((number/10*16)+(number%10));
+}
 
-static esp_err_t  i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr, size_t size)
+static esp_err_t i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr, size_t size)
 {
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
@@ -31,7 +36,18 @@ static esp_err_t  i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr, s
     i2c_cmd_link_delete(cmd);
     return ret;
 }
-
+static esp_err_t i2c_master_read_slave(i2c_port_t i2c_num, uint8_t start_add, uint8_t *data_r, size_t size)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    TEST_ESP_OK(i2c_master_write_byte(cmd, ( ESP_SLAVE_ADDR << 1 ) | READ_BIT, ACK_CHECK_EN));
+    TEST_ESP_OK(i2c_master_write_byte(cmd, start_add, ACK_CHECK_EN));
+    TEST_ESP_OK(i2c_master_read(cmd, data_r, size, ACK_CHECK_EN));
+    TEST_ESP_OK(i2c_master_stop(cmd));
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 5000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
 void app_main()
 {
     i2c_config_t i2c_config =
@@ -54,12 +70,22 @@ void app_main()
     uint8_t *data_write = (uint8_t *) malloc(256);
     uint8_t *data_read = (uint8_t *) malloc(256);
 
-    data_write[0]=0x00;
-    i2c_master_write_slave(I2C_NUM_1, data_write, 2);
-
+    for(int i=0; i<7;i++)
+    {
+        data_write[0]=0x00|dec_to_bcd(i);
+        data_write[1]=dec_to_bcd(0x00);
+        i2c_master_write_slave(I2C_NUM_1, data_write, 2);
+    }
     while(1)
     {
-        
+        i2c_master_read_slave(I2C_NUM_1, 0x00,data_read,7);
+        second=bcd_to_dec(data_read[0] & 0x7F);
+        minute=bcd_to_dec(data_read[1]);
+        hour=bcd_to_dec(data_read[2] & 0x3F);
+        day=bcd_to_dec(data_read[3]);
+        date=bcd_to_dec(data_read[4]);
+        month=bcd_to_dec(data_read[5]);
+        year=bcd_to_dec(data_read[6]);       
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         
     }
